5f48abe63976fec9105cedfcdf7c44c53aa6c958
ajj <a@ajweb.eu>
2014-05-06 16:38:21 +0200

    Removed very_exists argument on URLField and changed user foreign key to use settings.AUTH_USER_MODEL

--- cas_provider/models.py
+++ cas_provider/models.py
@@ -1,9 +1,9 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.conf import settings
 
 class ServiceTicket(models.Model):
-    user = models.ForeignKey(User)
-    service = models.URLField(verify_exists=False)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL)
+    service = models.URLField()
     ticket = models.CharField(max_length=256)
     created = models.DateTimeField(auto_now=True)
     
